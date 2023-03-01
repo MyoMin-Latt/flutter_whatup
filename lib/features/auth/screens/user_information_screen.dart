64493f032afb888555c2ce2f5db33c83585c7403
MyoMinLatt <myominlatt.sbs@gmail.com>
2023-03-01 16:20:48 +0630

    user infor screen ui

--- lib/features/auth/screens/user_information_screen.dart
+++ lib/features/auth/screens/user_information_screen.dart
@@ -1,11 +1,34 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class UserInformationScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:whatsapp_ui/common/utils/utils.dart';
+
+class UserInformationScreen extends StatefulWidget {
   static const String routeName = '/user-information';
   const UserInformationScreen({Key? key}) : super(key: key);
 
   @override
+  State<UserInformationScreen> createState() => _UserInformationScreenState();
+}
+
+class _UserInformationScreenState extends State<UserInformationScreen> {
+  final nameController = TextEditingController();
+  File? image;
+  @override
+  void dispose() {
+    super.dispose();
+    nameController.clear();
+  }
+
+  void selectImage() async {
+    image = await pickImageFromGallery(context);
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
       body: SafeArea(
           child: Center(
@@ -13,12 +36,39 @@ class UserInformationScreen extends StatelessWidget {
           children: [
             Stack(
               children: [
-                CircleAvatar(
-                  backgroundImage: NetworkImage(
-                      'https://pixabay.com/vectors/blank-profile-picture-mystery-man-973460'),
-                  radius: 30,
+                image == null
+                    ? const CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            'https://static.vecteezy.com/system/resources/previews/002/002/403/large_2x/man-with-beard-avatar-character-isolated-icon-free-vector.jpg'),
+                        radius: 64,
+                      )
+                    : CircleAvatar(
+                        backgroundImage: FileImage(image!),
+                        radius: 64,
+                      ),
+                Positioned(
+                  bottom: -10,
+                  left: 80,
+                  child: IconButton(
+                    onPressed: () => selectImage(),
+                    icon: const Icon(Icons.add_a_photo),
+                  ),
                 ),
-                IconButton(onPressed: () {}, icon: Icon(Icons.add_a_photo))
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                  width: size.width * 0.85,
+                  padding: const EdgeInsets.all(20),
+                  child: TextField(
+                    controller: nameController,
+                    decoration: const InputDecoration(
+                      hintText: 'Enter your name',
+                    ),
+                  ),
+                ),
+                IconButton(onPressed: () {}, icon: const Icon(Icons.done))
               ],
             )
           ],
